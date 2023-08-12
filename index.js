b1fd70f6beadb1e52cded7235d7f4198c4830ff3
goutham <goutham@gouthams-MacBook-Air-6.local>
2023-08-12 18:39:34 +0530

    first text

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express=require("express");
 const app=express();
 const port=process.env.PORT || 8081
 
+app.get("/",(req,res)=>{
+    res.send(" This is my first text in chat server");
+})
 app.listen(port,()=>{
     console.log(`Server started on Port ${port}`);
 })
