d707c919e86008e2488a1a9bb264af04716783af
goutham <goutham@gouthams-MacBook-Air-6.local>
2023-08-12 19:25:37 +0530

    first clientpage

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express=require("express");
 const app=express();
+const path=require("path");
 const port=process.env.PORT || 8081
 
+app.use(express.static("public"));
+
 app.get("/",(req,res)=>{
-    res.send(" This is my first text in chat server");
+    res.sendFile(path.join(__dirname,"public","index.html"));
 })
 app.listen(port,()=>{
     console.log(`Server started on Port ${port}`);
