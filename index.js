28c705fb2cda168a422f9bf33a910bab9753b9c0
goutham <goutham@gouthams-MacBook-Air-6.local>
2023-08-12 18:15:21 +0530

    second commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const app=express();
+const port=process.env.PORT || 8081
 
-
-app.listen(3000,()=>{
-    console.log("Hello");
+app.listen(port,()=>{
+    console.log(`Server started on Port ${port}`);
 })
